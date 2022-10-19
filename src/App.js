62156e08e5dfd875fdab3a0c8aa70ae94f137796
PhuongUyen0712 <75712660+PhuongUyen0712@users.noreply.github.com>
2022-10-19 21:27:26 +0700

    Load meme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Button, Row, Col } from "antd";
+import { useEffect, useState } from "react";
+const api_meme = "https://api.imgflip.com/get_memes";
+function ImageMeme(props) {
+  const { src, alt, key } = props;
+  return <img key={key} src={src} className="meme_image" alt={alt} />;
+}
 function App() {
+  const [memeArray, setMemeArray] = useState([]);
+  const getImage = () => {
+    fetch(api_meme)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setMemeArray(data.data.memes);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+  useEffect(() => {
+    getImage();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="App-header">Meme List</h3>
+      <p className="desc">Build and website that has a button and a gallery.</p>
+      <Button type="primary" onClick={(event) => getImage(event)}>
+        Load images
+      </Button>
+      <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 16]}>
+        {memeArray.length !== 0 &&
+          memeArray.map((item, index) => {
+            return (
+              <Col span={12} className="gutter-row image-item" key={`${index}`}>
+                <ImageMeme src={item.url} alt={item.name} />
+              </Col>
+            );
+          })}
+      </Row>
     </div>
   );
 }
